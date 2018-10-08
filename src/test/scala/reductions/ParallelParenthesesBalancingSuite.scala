5ad7b69bdab351d6e4e530ec4b54afd9fc51f0b3
Dima Koltovich <steklopod@gmail.com>
2018-10-08 10:16:52 +0300

    Reductions and Prefix Sums - need to implement some functions

---
+++ src/test/scala/reductions/ParallelParenthesesBalancingSuite.scala
@@ -0,0 +1,45 @@
+package reductions
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+import reductions.ParallelParenthesesBalancing._
+
+@RunWith(classOf[JUnitRunner])
+class ParallelParenthesesBalancingSuite extends FunSuite {
+
+  test("balance should work for empty string") {
+    def check(input: String, expected: Boolean) =
+      assert(balance(input.toArray) == expected,
+        s"balance($input) should be $expected")
+
+    check("", true)
+  }
+
+  test("balance should work for string of length 1") {
+    def check(input: String, expected: Boolean) =
+      assert(balance(input.toArray) == expected,
+        s"balance($input) should be $expected")
+
+    check("(", false)
+    check(")", false)
+    check(".", true)
+  }
+
+  test("balance should work for string of length 2") {
+    def check(input: String, expected: Boolean) =
+      assert(balance(input.toArray) == expected,
+        s"balance($input) should be $expected")
+
+    check("()", true)
+    check(")(", false)
+    check("((", false)
+    check("))", false)
+    check(".)", false)
+    check(".(", false)
+    check("(.", false)
+    check(").", false)
+  }
+
+
+}
